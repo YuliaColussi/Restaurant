dabd247449d549e9846e2d43f00cf43c2ebad64a
i.colussi <yuliabelova2307@gmail.com>
2023-02-19 12:50:15 +0100

    installed tailwind

---
+++ restaurant/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
